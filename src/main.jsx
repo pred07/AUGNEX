4ac3d0fde069df6d24ec0a01e0762d4fcd4f89e1
Your Name <you@example.com>
2025-12-17 11:56:59 +0530

    Refactor: Rename index.css to app.css to bust build cache

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './app.css'
 import App from './App.jsx'
 
 import ErrorBoundary from './components/ErrorBoundary';
