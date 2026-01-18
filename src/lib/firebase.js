caaa2ca42270c7ee3abc9f19193d605366055d73
Your Name <you@example.com>
2026-01-18 22:00:39 +0530

    Switch to new Firebase project (augnex-8017e)

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,15 +3,16 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA7Z8xuoghIyliE6tobbmOPtf4YeHFhcaE",
-    authDomain: "project11-fddd7.firebaseapp.com",
-    projectId: "project11-fddd7",
-    storageBucket: "project11-fddd7.firebasestorage.app",
-    messagingSenderId: "92957184996",
-    appId: "1:92957184996:web:49367a9cc086cc73692df1"
+    apiKey: "AIzaSyAQ3SAwcbHuUkT6K1B1IzZ7Mp20u_DIBKc",
+    authDomain: "augnex-8017e.firebaseapp.com",
+    projectId: "augnex-8017e",
+    storageBucket: "augnex-8017e.firebasestorage.app",
+    messagingSenderId: "297906817052",
+    appId: "1:297906817052:web:0a42fa1ed50b1f234037d9",
+    measurementId: "G-D895QBX6ZQ"
 };
 
-console.log("Initializing Firebase with config:", firebaseConfig);
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
